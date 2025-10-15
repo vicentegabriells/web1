90172a69f2e947aad1ce7c42a13e0c222a04af61
Vicente Souza <vicentesouza.dev@gmail.com>
2025-10-14 21:24:25 -0300

    commit 1

---
+++ src/main/java/br/com/milhas/gerenciador/GerenciadorMilhasApplication.java
@@ -0,0 +1,13 @@
+package br.com.milhas.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorMilhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorMilhasApplication.class, args);
+	}
+
+}
